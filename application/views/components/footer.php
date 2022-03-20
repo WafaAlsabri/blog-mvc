a16a524ed7ab98dc2685afe0dcbb59eaca3a0183
WafaAlsabri <wafaalsabri23@gmail.com>
2022-03-20 09:03:33 +0300

    first commit

---
+++ application/views/components/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="copyright-text"> 
+                <p>Copyright © 2022 Wafa Alsabri</p></div>
+            </div>
+          </div>
+     </footer>
+</body>
+</html>
